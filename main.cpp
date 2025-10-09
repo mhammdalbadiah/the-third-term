379103e0d4a6f391106672da87e17a3484aa46a8
mhammd AL-badiah <mhammdqqpp@gmail.com>
2025-10-09 20:45:29 +0300

    dynamic variabel !

--- main.cpp
+++ main.cpp
@@ -1,32 +1,76 @@
-
-#include <iostream>
-using namespace std ;
  
-int fac(int x){
-   if ( x <= 1 ) return 1;
-   return  x  *  fac(x-1);
-}
+#include <iostream>
+ #include <string>
+#include "uni.h"
+#include <vector>
 
-long long fib (int n ){
-   
-   if (n==0)return 0;
-   if (n==1)return 1;
-   long long a = 0 , b= 1 ,temp ;
-   
-   for ( int i=2; i <= n ; i++ ){
-       
-       temp = a+b;
-       a=b;
-       b=temp;
-   }
-   return b;
-}
+using namespace std ;
 
 
 int main (){
-   cout << fib(7) << endl;
-   return 0;
+    
+    int x = 0 ;
+    
+    cout << "enter the size of the array : " << endl;
+    cin >> x;
+    
+    int *ptx = new int[x];
+    
+    cout << "enter the Data : " << endl;
+    
+    for (int i = 0; i<x;i++ ){
+        cin >> ptx[i] ;
+    }
+    
+    for (int i = 0; i<x;i++ ){
+        cout << ptx[i] <<endl;
+    }
+    
+    
+    
+    
+    
+     
+    
+    /*uni a[5];3
+    
+    uni az = {101,20,"Mhammd","IT"};
+    az.display();
+   int i =0 ,aa;
+  string n,m ;
+
+    for (int ii=0;ii<5;ii++){
+        cout << "enter your ID : " ;
+        cin >> i;
+        a[ii].set_id( i);
+        cout << endl;
+        cout << "enter your AGE : ";
+        cin >> aa;
+        a[ii].set_age(aa);
+        cout << endl;
+        cout <<"enter your NAME :" ;
+        cin >> n ;
+        a[ii].set_name( n);
+        cout << endl;
+        cout << "enter your MAJOR :" ;
+        cin >> m ;
+        a[ii].set_major( m);
+    }
+    cout << endl << endl << endl;
+    int x=0;
+    cout << "enter 1 to print : " << endl;
+    cin >> x;
+    if ( x == 1 ) {
+        
+        for (int ii=0;ii<5;ii++){
+            a[ii].display();
+        }
+        
+        
+        
+    }
+  
+   */
+    
+    return 0;
 }
-
-
-
